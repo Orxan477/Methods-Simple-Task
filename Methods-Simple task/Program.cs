127c9180d9a4cff3bcc6efe978ed5c61597c0fdd
Orxan Qanbarov <41814449+Orxan477@users.noreply.github.com>
2021-10-27 12:55:48 +0400

    Methods Task

---
+++ Methods-Simple task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Methods_Simple_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 2002, 4, 5 };
+            int[] arr1 = { 1223, 785, 54, 4, 5 };
+            if (massiv(arr) > massiv(arr1))
+            {
+                Console.WriteLine(massiv(arr));
+                return;
+            }               
+                Console.WriteLine(massiv(arr1));
+        }
+        static int massiv(int[] m)
+        {
+            int max = m[0];
+
+            foreach (var item in m)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+    }
+
+}
